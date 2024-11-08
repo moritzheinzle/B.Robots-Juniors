bf473a408f20a556f85d25d8b7d20ae2017dec18
Moritz Heinzle <moritzheinzle@icloud.com>
2024-11-08 22:08:33 +0100

    Added CameraInterrupt .h/.cpp

--- Files/ArduinoMega/src/main.cpp
+++ Files/ArduinoMega/src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
+#include <CameraInterrupt.h>
 
-// put function declarations here:
-int myFunction(int, int);
+CameraInterrupt cameraInterrupt;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    cameraInterrupt.begin();  // Library initialisieren
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Die Logik ist vollständig in der Interrupt-Handler-Methode untergebracht
+    // Hauptloop bleibt leer, da der Interrupt die Ausgabe übernimmt
 }
